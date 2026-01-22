7a728b2f0adf052b88fe4bdaeff8496830caa30f
Damir Vafin <damirvafinnailevich@gmail.com>
2026-01-22 16:24:28 +0300

    feat: добавить валидацию в FilmController

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -2,6 +2,7 @@ package ru.yandex.practicum.filmorate.controller;
 
 import org.springframework.web.bind.annotation.*;
 import ru.yandex.practicum.filmorate.model.Film;
+import java.time.LocalDate;
 import java.util.*;
 
 @RestController
@@ -17,6 +18,7 @@ public class FilmController {
 
     @PostMapping
     public Film create(@RequestBody Film film) {
+        validate(film); // Валидация при создании
         film.setId(idCounter++);
         films.put(film.getId(), film);
         return film;
@@ -24,7 +26,23 @@ public class FilmController {
 
     @PutMapping
     public Film update(@RequestBody Film film) {
+        validate(film); // Валидация при обновлении
         films.put(film.getId(), film);
         return film;
+    }
+
+    private void validate(Film film) {
+        if (film.getName() == null || film.getName().isBlank()) {
+            throw new ValidationException("Название не может быть пустым");
+        }
+        if (film.getDescription() != null && film.getDescription().length() > 200) {
+            throw new ValidationException("Описание не может быть длиннее 200 символов");
+        }
+        if (film.getReleaseDate() == null || film.getReleaseDate().isBefore(LocalDate.of(1895, 12, 28))) {
+            throw new ValidationException("Дата релиза не может быть раньше 28 декабря 1895 года");
+        }
+        if (film.getDuration() <= 0) {
+            throw new ValidationException("Продолжительность должна быть положительной");
+        }
     }
 }
