dc727d691c1cb8985a027b348d125ce2a5688e06
Damir Vafin <damirvafinnailevich@gmail.com>
2026-01-22 16:18:42 +0300

    feat: создать FilmController

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -1,7 +1,30 @@
 package ru.yandex.practicum.filmorate.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import ru.yandex.practicum.filmorate.model.Film;
+import java.util.*;
 
 @RestController
+@RequestMapping("/films")
 public class FilmController {
+    private final Map<Integer, Film> films = new HashMap<>();
+    private int idCounter = 1;
+
+    @GetMapping
+    public List<Film> findAll() {
+        return new ArrayList<>(films.values());
+    }
+
+    @PostMapping
+    public Film create(@RequestBody Film film) {
+        film.setId(idCounter++);
+        films.put(film.getId(), film);
+        return film;
+    }
+
+    @PutMapping
+    public Film update(@RequestBody Film film) {
+        films.put(film.getId(), film);
+        return film;
+    }
 }
