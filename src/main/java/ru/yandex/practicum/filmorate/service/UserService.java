f75f61b617c174b3f26dfd547df4b663340763fe
Damir Vafin <damirvafinnailevich@gmail.com>
2026-02-11 22:44:28 +0300

    feat: реализовать UserService с методами для работы с друзьями

--- src/main/java/ru/yandex/practicum/filmorate/service/UserService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/UserService.java
@@ -1,4 +1,74 @@
 package ru.yandex.practicum.filmorate.service;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import ru.yandex.practicum.filmorate.controller.NotFoundException;
+import ru.yandex.practicum.filmorate.model.User;
+import ru.yandex.practicum.filmorate.storage.user.UserStorage;
+
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
 public class UserService {
+
+    private final UserStorage userStorage;
+
+    public void addFriend(int userId, int friendId) {
+        log.info("Добавление в друзья: пользователь {} добавляет пользователя {}", userId, friendId);
+
+        User user = getUserOrThrow(userId);
+        User friend = getUserOrThrow(friendId);
+
+        user.getFriends().add(friendId);
+        friend.getFriends().add(userId);
+
+        log.info("Пользователи {} и {} теперь друзья", userId, friendId);
+    }
+
+    public void removeFriend(int userId, int friendId) {
+        log.info("Удаление из друзей: пользователь {} удаляет пользователя {}", userId, friendId);
+
+        User user = getUserOrThrow(userId);
+        User friend = getUserOrThrow(friendId);
+
+        user.getFriends().remove(friendId);
+        friend.getFriends().remove(userId);
+
+        log.info("Пользователи {} и {} больше не друзья", userId, friendId);
+    }
+
+    public List<User> getFriends(int userId) {
+        log.info("Получение списка друзей пользователя {}", userId);
+
+        User user = getUserOrThrow(userId);
+
+        return user.getFriends().stream()
+                .map(this::getUserOrThrow)
+                .collect(Collectors.toList());
+    }
+
+    public List<User> getCommonFriends(int userId, int otherUserId) {
+        log.info("Получение общих друзей пользователей {} и {}", userId, otherUserId);
+
+        User user = getUserOrThrow(userId);
+        User otherUser = getUserOrThrow(otherUserId);
+
+        Set<Integer> commonFriendIds = user.getFriends().stream()
+                .filter(otherUser.getFriends()::contains)
+                .collect(Collectors.toSet());
+
+        return commonFriendIds.stream()
+                .map(this::getUserOrThrow)
+                .collect(Collectors.toList());
+    }
+
+    private User getUserOrThrow(int id) {
+        return userStorage.getById(id)
+                .orElseThrow(() -> new NotFoundException("Пользователь с id=" + id + " не найден"));
+    }
 }
