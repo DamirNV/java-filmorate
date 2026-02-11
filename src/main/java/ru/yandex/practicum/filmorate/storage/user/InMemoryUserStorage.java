b4fc7d5a729ae664235d481c3e9b85f0337093f1
Damir Vafin <damirvafinnailevich@gmail.com>
2026-02-11 22:27:36 +0300

    feat: добавить InMemoryUserStorage с реализацией методов UserStorage

--- src/main/java/ru/yandex/practicum/filmorate/storage/user/InMemoryUserStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/user/InMemoryUserStorage.java
@@ -1,4 +1,54 @@
 package ru.yandex.practicum.filmorate.storage.user;
 
-public class InMemoryUserStorage {
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import ru.yandex.practicum.filmorate.model.User;
+import ru.yandex.practicum.filmorate.controller.NotFoundException;
+
+import java.util.*;
+
+@Slf4j
+@Component
+public class InMemoryUserStorage implements UserStorage {
+
+    private final Map<Integer, User> users = new HashMap<>();
+    private int idCounter = 1;
+
+    @Override
+    public User add(User user) {
+        user.setId(idCounter++);
+        users.put(user.getId(), user);
+        log.info("Пользователь добавлен в хранилище с id: {}", user.getId());
+        return user;
+    }
+
+    @Override
+    public User update(User user) {
+        if (!users.containsKey(user.getId())) {
+            throw new NotFoundException("Пользователь с id=" + user.getId() + " не найден");
+        }
+        users.put(user.getId(), user);
+        log.info("Пользователь с id {} обновлен в хранилище", user.getId());
+        return user;
+    }
+
+    @Override
+    public boolean delete(int id) {
+        User removed = users.remove(id);
+        if (removed != null) {
+            log.info("Пользователь с id {} удален из хранилища", id);
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public List<User> getAll() {
+        return new ArrayList<>(users.values());
+    }
+
+    @Override
+    public Optional<User> getById(int id) {
+        return Optional.ofNullable(users.get(id));
+    }
 }
