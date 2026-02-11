72e2f09efdd15856597b57aed8e1f69af74b44e6
Damir Vafin <damirvafinnailevich@gmail.com>
2026-02-11 22:31:14 +0300

    refactor: внедрить FilmStorage в FilmController

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -1,44 +1,41 @@
 package ru.yandex.practicum.filmorate.controller;
 
 import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 import ru.yandex.practicum.filmorate.model.Film;
-import java.util.*;
+import ru.yandex.practicum.filmorate.storage.film.FilmStorage;
+
+import java.util.List;
 
 @Slf4j
 @RestController
 @RequestMapping("/films")
+@RequiredArgsConstructor
 public class FilmController {
-    private final Map<Integer, Film> films = new HashMap<>();
-    private int idCounter = 1;
+
+    private final FilmStorage filmStorage;
 
     @GetMapping
     public List<Film> findAll() {
-        log.info("Получен запрос на получение всех фильмов. Текущее количество: {}", films.size());
-        return new ArrayList<>(films.values());
+        log.info("Получен запрос на получение всех фильмов");
+        return filmStorage.getAll();
     }
 
     @PostMapping
     public Film create(@Valid @RequestBody Film film) {
         log.info("Получен запрос на создание фильма: {}", film);
-        film.setId(idCounter++);
-        films.put(film.getId(), film);
-        log.info("Фильм успешно создан с id: {}", film.getId());
-        return film;
+        Film createdFilm = filmStorage.add(film);
+        log.info("Фильм успешно создан с id: {}", createdFilm.getId());
+        return createdFilm;
     }
 
     @PutMapping
     public Film update(@Valid @RequestBody Film film) {
         log.info("Получен запрос на обновление фильма с id: {}", film.getId());
-
-        if (film.getId() == 0 || !films.containsKey(film.getId())) {
-            throw new NotFoundException("Фильм с id=" + film.getId() + " не найден");
-        }
-
-        films.put(film.getId(), film);
-        log.info("Фильм с id {} успешно обновлен", film.getId());
-        return film;
+        Film updatedFilm = filmStorage.update(film);
+        log.info("Фильм с id {} успешно обновлен", updatedFilm.getId());
+        return updatedFilm;
     }
-
 }
