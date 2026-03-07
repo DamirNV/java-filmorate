f761a8e8a0d336a9fcc8f043c9a69e1f0b3ec437
Damir Vafin <damirvafinnailevich@gmail.com>
2026-03-07 23:13:24 +0300

    test: добавить тесты для моделей Genre, Mpa, Film, User

--- src/test/java/ru/yandex/practicum/filmorate/model/MpaTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/model/MpaTest.java
@@ -1,4 +1,108 @@
 package ru.yandex.practicum.filmorate.model;
 
-public class MpaTest {
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MpaTest {
+
+    @Test
+    void equals_SameId_ShouldReturnTrue() {
+        Mpa mpa1 = new Mpa();
+        mpa1.setId(1);
+        mpa1.setName("G");
+
+        Mpa mpa2 = new Mpa();
+        mpa2.setId(1);
+        mpa2.setName("PG-13");
+
+        assertEquals(mpa1, mpa2);
+    }
+
+    @Test
+    void equals_DifferentId_ShouldReturnFalse() {
+        Mpa mpa1 = new Mpa();
+        mpa1.setId(1);
+        mpa1.setName("G");
+
+        Mpa mpa2 = new Mpa();
+        mpa2.setId(2);
+        mpa2.setName("G");
+
+        assertNotEquals(mpa1, mpa2);
+    }
+
+    @Test
+    void equals_SameObject_ShouldReturnTrue() {
+        Mpa mpa = new Mpa();
+        mpa.setId(1);
+        mpa.setName("G");
+
+        assertEquals(mpa, mpa);
+    }
+
+    @Test
+    void equals_Null_ShouldReturnFalse() {
+        Mpa mpa = new Mpa();
+        mpa.setId(1);
+        mpa.setName("G");
+
+        assertNotEquals(null, mpa);
+    }
+
+    @Test
+    void equals_DifferentClass_ShouldReturnFalse() {
+        Mpa mpa = new Mpa();
+        mpa.setId(1);
+        mpa.setName("G");
+
+        String notAnMpa = "not an mpa";
+        assertNotEquals(mpa, notAnMpa);
+    }
+
+    @Test
+    void hashCode_SameId_ShouldBeEqual() {
+        Mpa mpa1 = new Mpa();
+        mpa1.setId(1);
+        mpa1.setName("G");
+
+        Mpa mpa2 = new Mpa();
+        mpa2.setId(1);
+        mpa2.setName("PG-13");
+
+        assertEquals(mpa1.hashCode(), mpa2.hashCode());
+    }
+
+    @Test
+    void hashCode_DifferentId_ShouldBeDifferent() {
+        Mpa mpa1 = new Mpa();
+        mpa1.setId(1);
+        mpa1.setName("G");
+
+        Mpa mpa2 = new Mpa();
+        mpa2.setId(2);
+        mpa2.setName("G");
+
+        assertNotEquals(mpa1.hashCode(), mpa2.hashCode());
+    }
+
+    @Test
+    void constructorAndGetters_ShouldWork() {
+        Mpa mpa = new Mpa();
+        mpa.setId(1);
+        mpa.setName("G");
+
+        assertEquals(1, mpa.getId());
+        assertEquals("G", mpa.getName());
+    }
+
+    @Test
+    void toString_ShouldNotBeEmpty() {
+        Mpa mpa = new Mpa();
+        mpa.setId(1);
+        mpa.setName("G");
+
+        assertNotNull(mpa.toString());
+        assertFalse(mpa.toString().isEmpty());
+    }
 }
--- src/test/java/ru/yandex/practicum/filmorate/model/UserTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/model/UserTest.java
@@ -1,4 +1,154 @@
 package ru.yandex.practicum.filmorate.model;
 
-public class UserTest {
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserTest {
+
+    private User user;
+    private Friendship friendship;
+
+    @BeforeEach
+    void setUp() {
+        user = new User();
+        user.setId(1);
+        user.setEmail("test@test.com");
+        user.setLogin("testlogin");
+        user.setName("Test User");
+        user.setBirthday(LocalDate.of(1990, 1, 1));
+        user.setFriendships(new HashSet<>());
+
+        friendship = new Friendship();
+        friendship.setUserId(1);
+        friendship.setFriendId(2);
+        friendship.setStatus(FriendshipStatus.CONFIRMED);
+    }
+
+    @Test
+    void constructorAndGetters_ShouldWork() {
+        assertEquals(1, user.getId());
+        assertEquals("test@test.com", user.getEmail());
+        assertEquals("testlogin", user.getLogin());
+        assertEquals("Test User", user.getName());
+        assertEquals(LocalDate.of(1990, 1, 1), user.getBirthday());
+        assertNotNull(user.getFriendships());
+        assertTrue(user.getFriendships().isEmpty());
+    }
+
+    @Test
+    void setters_ShouldWork() {
+        user.setId(2);
+        user.setEmail("new@test.com");
+        user.setLogin("newlogin");
+        user.setName("New User");
+        user.setBirthday(LocalDate.of(1991, 1, 1));
+
+        assertEquals(2, user.getId());
+        assertEquals("new@test.com", user.getEmail());
+        assertEquals("newlogin", user.getLogin());
+        assertEquals("New User", user.getName());
+        assertEquals(LocalDate.of(1991, 1, 1), user.getBirthday());
+    }
+
+    @Test
+    void friendships_ShouldBeMutable() {
+        user.getFriendships().add(friendship);
+        assertEquals(1, user.getFriendships().size());
+        assertTrue(user.getFriendships().contains(friendship));
+
+        user.getFriendships().clear();
+        assertTrue(user.getFriendships().isEmpty());
+    }
+
+    @Test
+    void equals_SameId_ShouldReturnTrue() {
+        User user2 = new User();
+        user2.setId(1);
+        user2.setEmail("different@test.com");
+
+        assertEquals(user, user2);
+    }
+
+    @Test
+    void equals_DifferentId_ShouldReturnFalse() {
+        User user2 = new User();
+        user2.setId(2);
+        user2.setEmail("test@test.com");
+
+        assertNotEquals(user, user2);
+    }
+
+    @Test
+    void equals_SameObject_ShouldReturnTrue() {
+        assertEquals(user, user);
+    }
+
+    @Test
+    void equals_Null_ShouldReturnFalse() {
+        assertNotEquals(null, user);
+    }
+
+    @Test
+    void hashCode_SameId_ShouldBeEqual() {
+        User user2 = new User();
+        user2.setId(1);
+
+        assertEquals(user.hashCode(), user2.hashCode());
+    }
+
+    @Test
+    void hashCode_DifferentId_ShouldBeDifferent() {
+        User user2 = new User();
+        user2.setId(2);
+
+        assertNotEquals(user.hashCode(), user2.hashCode());
+    }
+
+    @Test
+    void toString_ShouldNotBeEmpty() {
+        assertNotNull(user.toString());
+        assertFalse(user.toString().isEmpty());
+    }
+
+    @Test
+    void user_WithNullName_ShouldBeValid() {
+        user.setName(null);
+        assertNull(user.getName());
+    }
+
+    @Test
+    void user_WithEmptyName_ShouldBeValid() {
+        user.setName("");
+        assertEquals("", user.getName());
+    }
+
+    @Test
+    void user_WithNullFriendships_ShouldBeValid() {
+        user.setFriendships(null);
+        assertNull(user.getFriendships());
+    }
+
+    @Test
+    void friendship_ShouldWork() {
+        assertEquals(1, friendship.getUserId());
+        assertEquals(2, friendship.getFriendId());
+        assertEquals(FriendshipStatus.CONFIRMED, friendship.getStatus());
+    }
+
+    @Test
+    void friendship_Setters_ShouldWork() {
+        friendship.setUserId(3);
+        friendship.setFriendId(4);
+        friendship.setStatus(FriendshipStatus.CONFIRMED);
+
+        assertEquals(3, friendship.getUserId());
+        assertEquals(4, friendship.getFriendId());
+        assertEquals(FriendshipStatus.CONFIRMED, friendship.getStatus());
+    }
 }
