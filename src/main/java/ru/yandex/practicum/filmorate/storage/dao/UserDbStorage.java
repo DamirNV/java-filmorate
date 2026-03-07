40226d0d3413bf0b1719a9e63dcf7578798fa874
Damir Vafin <damirvafinnailevich@gmail.com>
2026-03-07 15:17:58 +0300

    feat: добавить UserDbStorage с реализацией UserStorage через JdbcTemplate

---
+++ src/main/java/ru/yandex/practicum/filmorate/storage/dao/UserDbStorage.java
@@ -0,0 +1,104 @@
+package ru.yandex.practicum.filmorate.storage.dao;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Primary;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
+import org.springframework.stereotype.Repository;
+import ru.yandex.practicum.filmorate.controller.NotFoundException;
+import ru.yandex.practicum.filmorate.model.User;
+import ru.yandex.practicum.filmorate.storage.user.UserStorage;
+
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.Statement;
+import java.util.List;
+import java.util.Optional;
+
+@Slf4j
+@Repository
+@Primary
+@RequiredArgsConstructor
+public class UserDbStorage implements UserStorage {
+
+    private final JdbcTemplate jdbcTemplate;
+
+    private final RowMapper<User> userRowMapper = (rs, rowNum) -> {
+        User user = new User();
+        user.setId(rs.getInt("user_id"));
+        user.setEmail(rs.getString("email"));
+        user.setLogin(rs.getString("login"));
+        user.setName(rs.getString("name"));
+        user.setBirthday(rs.getDate("birthday").toLocalDate());
+        return user;
+    };
+
+    @Override
+    public User add(User user) {
+        String sql = "INSERT INTO users (email, login, name, birthday) VALUES (?, ?, ?, ?)";
+        KeyHolder keyHolder = new GeneratedKeyHolder();
+
+        jdbcTemplate.update(connection -> {
+            PreparedStatement ps = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+            ps.setString(1, user.getEmail());
+            ps.setString(2, user.getLogin());
+            ps.setString(3, user.getName());
+            ps.setDate(4, Date.valueOf(user.getBirthday()));
+            return ps;
+        }, keyHolder);
+
+        Number key = keyHolder.getKey();
+        if (key != null) {
+            user.setId(key.intValue());
+        }
+
+        log.info("Пользователь добавлен в БД с id: {}", user.getId());
+        return user;
+    }
+
+    @Override
+    public User update(User user) {
+        String sql = "UPDATE users SET email = ?, login = ?, name = ?, birthday = ? WHERE user_id = ?";
+        int updated = jdbcTemplate.update(sql,
+                user.getEmail(),
+                user.getLogin(),
+                user.getName(),
+                Date.valueOf(user.getBirthday()),
+                user.getId());
+
+        if (updated == 0) {
+            throw new NotFoundException("Пользователь с id=" + user.getId() + " не найден");
+        }
+
+        log.info("Пользователь с id {} обновлен в БД", user.getId());
+        return user;
+    }
+
+    @Override
+    public boolean delete(int id) {
+        String sql = "DELETE FROM users WHERE user_id = ?";
+        int deleted = jdbcTemplate.update(sql, id);
+        if (deleted > 0) {
+            log.info("Пользователь с id {} удален из БД", id);
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public List<User> getAll() {
+        String sql = "SELECT * FROM users";
+        log.debug("Запрос всех пользователей из БД");
+        return jdbcTemplate.query(sql, userRowMapper);
+    }
+
+    @Override
+    public Optional<User> getById(int id) {
+        String sql = "SELECT * FROM users WHERE user_id = ?";
+        List<User> users = jdbcTemplate.query(sql, userRowMapper, id);
+        return users.stream().findFirst();
+    }
+}
