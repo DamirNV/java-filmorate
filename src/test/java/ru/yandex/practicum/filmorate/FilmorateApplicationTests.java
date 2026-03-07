67340d6352f54203c18400fb51076ae6c7920636
Damir Vafin <damirvafinnailevich@gmail.com>
2026-03-08 00:22:53 +0300

    test: добавить профиль test для изолированной БД

--- src/test/java/ru/yandex/practicum/filmorate/FilmorateApplicationTests.java
+++ src/test/java/ru/yandex/practicum/filmorate/FilmorateApplicationTests.java
@@ -2,12 +2,13 @@ package ru.yandex.practicum.filmorate;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class FilmorateApplicationTests {
 
     @Test
     void contextLoads() {
     }
-
 }
