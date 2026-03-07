8d2cf1561fe410d773b69fde2abf061116632cb6
Damir Vafin <damirvafinnailevich@gmail.com>
2026-03-07 19:44:17 +0300

    refactor: обновить UserController для работы с DTO

--- src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -4,7 +4,9 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
-import ru.yandex.practicum.filmorate.model.User;
+import ru.yandex.practicum.filmorate.dto.user.CreateUserRequest;
+import ru.yandex.practicum.filmorate.dto.user.UpdateUserRequest;
+import ru.yandex.practicum.filmorate.dto.user.UserResponse;
 import ru.yandex.practicum.filmorate.service.UserService;
 
 import java.util.List;
@@ -18,39 +20,27 @@ public class UserController {
     private final UserService userService;
 
     @GetMapping
-    public List<User> findAll() {
+    public List<UserResponse> findAll() {
         log.info("Получен запрос на получение всех пользователей");
         return userService.getAllUsers();
     }
 
     @GetMapping("/{id}")
-    public User getUserById(@PathVariable int id) {
+    public UserResponse getUserById(@PathVariable int id) {
         log.info("GET /users/{}", id);
         return userService.getUserById(id);
     }
 
     @PostMapping
-    public User create(@Valid @RequestBody User user) {
-        log.info("Получен запрос на создание пользователя: {}", user);
-
-        if (user.getName() == null || user.getName().isBlank()) {
-            user.setName(user.getLogin());
-            log.debug("Имя пользователя пустое, установлен логин: {}", user.getLogin());
-        }
-
-        return userService.createUser(user);
+    public UserResponse create(@Valid @RequestBody CreateUserRequest request) {
+        log.info("Получен запрос на создание пользователя: {}", request);
+        return userService.createUser(request);
     }
 
     @PutMapping
-    public User update(@Valid @RequestBody User user) {
-        log.info("Получен запрос на обновление пользователя с id: {}", user.getId());
-
-        if (user.getName() == null || user.getName().isBlank()) {
-            user.setName(user.getLogin());
-            log.debug("Имя пользователя пустое, установлен логин: {}", user.getLogin());
-        }
-
-        return userService.updateUser(user);
+    public UserResponse update(@Valid @RequestBody UpdateUserRequest request) {
+        log.info("Получен запрос на обновление пользователя с id: {}", request.getId());
+        return userService.updateUser(request);
     }
 
     @PutMapping("/{id}/friends/{friendId}")
@@ -72,19 +62,19 @@ public class UserController {
     }
 
     @GetMapping("/{id}/friends")
-    public List<User> getFriends(@PathVariable int id) {
+    public List<UserResponse> getFriends(@PathVariable int id) {
         log.info("GET /users/{}/friends - получение списка друзей", id);
         return userService.getFriends(id);
     }
 
     @GetMapping("/{id}/friends/pending")
-    public List<User> getPendingRequests(@PathVariable int id) {
+    public List<UserResponse> getPendingRequests(@PathVariable int id) {
         log.info("GET /users/{}/friends/pending - получение входящих запросов", id);
         return userService.getPendingRequests(id);
     }
 
     @GetMapping("/{id}/friends/common/{otherId}")
-    public List<User> getCommonFriends(@PathVariable int id, @PathVariable int otherId) {
+    public List<UserResponse> getCommonFriends(@PathVariable int id, @PathVariable int otherId) {
         log.info("GET /users/{}/friends/common/{} - получение общих друзей", id, otherId);
         return userService.getCommonFriends(id, otherId);
     }
