00c6ae1d83dbc2979e3c778817b805bad243f039
Damir Vafin <damirvafinnailevich@gmail.com>
2026-03-07 22:51:03 +0300

    test: обновить тесты контроллеров для работы с DTO

--- src/test/java/ru/yandex/practicum/filmorate/controller/FilmControllerTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/controller/FilmControllerTest.java
@@ -8,7 +8,9 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-import ru.yandex.practicum.filmorate.model.Film;
+import ru.yandex.practicum.filmorate.dto.film.CreateFilmRequest;
+import ru.yandex.practicum.filmorate.dto.film.FilmResponse;
+import ru.yandex.practicum.filmorate.dto.film.UpdateFilmRequest;
 import ru.yandex.practicum.filmorate.model.Genre;
 import ru.yandex.practicum.filmorate.model.Mpa;
 import ru.yandex.practicum.filmorate.service.FilmService;
@@ -20,7 +22,6 @@ import java.util.Set;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyInt;
-import static org.mockito.ArgumentMatchers.anySet;
 import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
@@ -38,11 +39,14 @@ class FilmControllerTest {
     @MockBean
     private FilmService filmService;
 
-    private Film validFilm;
+    private CreateFilmRequest createRequest;
+    private FilmResponse filmResponse;
+    private UpdateFilmRequest updateRequest;
     private Mpa mpa;
-    private Genre genre1;
-    private Genre genre2;
-    private Set<Genre> genres;
+    private CreateFilmRequest.GenreId genreId1;
+    private CreateFilmRequest.GenreId genreId2;
+    private Set<CreateFilmRequest.GenreId> genreIds;
+    private Set<Genre> responseGenres;
 
     @BeforeEach
     void setUp() {
@@ -50,53 +54,77 @@ class FilmControllerTest {
         mpa.setId(1);
         mpa.setName("PG-13");
 
-        genre1 = new Genre();
-        genre1.setId(1);
-        genre1.setName("Комедия");
+        genreId1 = new CreateFilmRequest.GenreId();
+        genreId1.setId(1);
 
-        genre2 = new Genre();
-        genre2.setId(2);
-        genre2.setName("Драма");
+        genreId2 = new CreateFilmRequest.GenreId();
+        genreId2.setId(2);
 
-        genres = new LinkedHashSet<>();
-        genres.add(genre1);
-        genres.add(genre2);
+        genreIds = new LinkedHashSet<>();
+        genreIds.add(genreId1);
+        genreIds.add(genreId2);
 
-        validFilm = new Film();
-        validFilm.setName("Valid Film");
-        validFilm.setDescription("A valid film description");
-        validFilm.setReleaseDate(LocalDate.of(2000, 1, 1));
-        validFilm.setDuration(120);
-        validFilm.setMpa(mpa);
-        validFilm.setGenres(genres);
+        Genre responseGenre1 = new Genre();
+        responseGenre1.setId(1);
+        responseGenre1.setName("Комедия");
+
+        Genre responseGenre2 = new Genre();
+        responseGenre2.setId(2);
+        responseGenre2.setName("Драма");
+
+        responseGenres = new LinkedHashSet<>();
+        responseGenres.add(responseGenre1);
+        responseGenres.add(responseGenre2);
+
+        createRequest = new CreateFilmRequest();
+        createRequest.setName("Valid Film");
+        createRequest.setDescription("A valid film description");
+        createRequest.setReleaseDate(LocalDate.of(2000, 1, 1));
+        createRequest.setDuration(120);
+        createRequest.setMpa(mpa);
+        createRequest.setGenres(genreIds);
+
+        filmResponse = new FilmResponse();
+        filmResponse.setId(1L);
+        filmResponse.setName("Valid Film");
+        filmResponse.setDescription("A valid film description");
+        filmResponse.setReleaseDate(LocalDate.of(2000, 1, 1));
+        filmResponse.setDuration(120);
+        filmResponse.setMpa(mpa);
+        filmResponse.setGenres(responseGenres);
+        filmResponse.setLikes(Set.of());
+
+        updateRequest = new UpdateFilmRequest();
+        updateRequest.setId(1L);
+        updateRequest.setName("Updated Film");
+        updateRequest.setDescription("Updated description");
+        updateRequest.setReleaseDate(LocalDate.of(2000, 1, 1));
+        updateRequest.setDuration(120);
+        updateRequest.setMpa(mpa);
+        updateRequest.setGenres(genreIds);
     }
 
     @Test
-    void createValidFilmShouldReturnOk() throws Exception {
-        Film createdFilm = new Film();
-        createdFilm.setId(1);
-        createdFilm.setName("Valid Film");
-        createdFilm.setDescription("A valid film description");
-        createdFilm.setReleaseDate(LocalDate.of(2000, 1, 1));
-        createdFilm.setDuration(120);
-        createdFilm.setMpa(mpa);
-        createdFilm.setGenres(genres);
-
-        when(filmService.createFilm(any(Film.class))).thenReturn(createdFilm);
+    void createFilm_ShouldReturnCreatedFilm() throws Exception {
+        when(filmService.createFilm(any(CreateFilmRequest.class))).thenReturn(filmResponse);
 
         mockMvc.perform(post("/films")
                         .contentType(MediaType.APPLICATION_JSON)
-                        .content(objectMapper.writeValueAsString(validFilm)))
+                        .content(objectMapper.writeValueAsString(createRequest)))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.id").value(1))
                 .andExpect(jsonPath("$.name").value("Valid Film"))
                 .andExpect(jsonPath("$.mpa.id").value(1))
-                .andExpect(jsonPath("$.genres[0].id").value(1));
+                .andExpect(jsonPath("$.mpa.name").value("PG-13"))
+                .andExpect(jsonPath("$.genres[0].id").value(1))
+                .andExpect(jsonPath("$.genres[0].name").value("Комедия"))
+                .andExpect(jsonPath("$.genres[1].id").value(2))
+                .andExpect(jsonPath("$.genres[1].name").value("Драма"));
     }
 
     @Test
-    void getAllFilmsShouldReturnOk() throws Exception {
-        when(filmService.getAllFilms()).thenReturn(List.of(validFilm));
+    void getAllFilms_ShouldReturnList() throws Exception {
+        when(filmService.getAllFilms()).thenReturn(List.of(filmResponse));
 
         mockMvc.perform(get("/films"))
                 .andExpect(status().isOk())
@@ -105,176 +133,13 @@ class FilmControllerTest {
     }
 
     @Test
-    void createFilmWithEmptyNameShouldReturnBadRequest() throws Exception {
-        validFilm.setName("");
-
-        mockMvc.perform(post("/films")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(objectMapper.writeValueAsString(validFilm)))
-                .andExpect(status().isBadRequest())
-                .andExpect(jsonPath("$.error").exists());
-    }
-
-    @Test
-    void createFilmWithNullNameShouldReturnBadRequest() throws Exception {
-        validFilm.setName(null);
-
-        mockMvc.perform(post("/films")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(objectMapper.writeValueAsString(validFilm)))
-                .andExpect(status().isBadRequest())
-                .andExpect(jsonPath("$.error").exists());
-    }
-
-    @Test
-    void createFilmWithBlankNameShouldReturnBadRequest() throws Exception {
-        validFilm.setName("   ");
-
-        mockMvc.perform(post("/films")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(objectMapper.writeValueAsString(validFilm)))
-                .andExpect(status().isBadRequest())
-                .andExpect(jsonPath("$.error").exists());
-    }
-
-    @Test
-    void createFilmWithTooLongDescriptionShouldReturnBadRequest() throws Exception {
-        validFilm.setDescription("A".repeat(201));
-
-        mockMvc.perform(post("/films")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(objectMapper.writeValueAsString(validFilm)))
-                .andExpect(status().isBadRequest())
-                .andExpect(jsonPath("$.error").exists());
-    }
-
-    @Test
-    void createFilmWithExactly200CharDescriptionShouldReturnOk() throws Exception {
-        validFilm.setDescription("A".repeat(200));
-        when(filmService.createFilm(any(Film.class))).thenReturn(validFilm);
-
-        mockMvc.perform(post("/films")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(objectMapper.writeValueAsString(validFilm)))
-                .andExpect(status().isOk());
-    }
-
-    @Test
-    void createFilmWithOldReleaseDateShouldReturnBadRequest() throws Exception {
-        validFilm.setReleaseDate(LocalDate.of(1895, 12, 27));
-
-        mockMvc.perform(post("/films")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(objectMapper.writeValueAsString(validFilm)))
-                .andExpect(status().isBadRequest())
-                .andExpect(jsonPath("$.error").exists());
-    }
-
-    @Test
-    void createFilmWithExactMinReleaseDateShouldReturnOk() throws Exception {
-        validFilm.setReleaseDate(LocalDate.of(1895, 12, 28));
-        when(filmService.createFilm(any(Film.class))).thenReturn(validFilm);
-
-        mockMvc.perform(post("/films")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(objectMapper.writeValueAsString(validFilm)))
-                .andExpect(status().isOk());
-    }
-
-    @Test
-    void createFilmWithNullReleaseDateShouldReturnBadRequest() throws Exception {
-        validFilm.setReleaseDate(null);
-
-        mockMvc.perform(post("/films")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(objectMapper.writeValueAsString(validFilm)))
-                .andExpect(status().isBadRequest())
-                .andExpect(jsonPath("$.error").exists());
-    }
-
-    @Test
-    void createFilmWithNegativeDurationShouldReturnBadRequest() throws Exception {
-        validFilm.setDuration(-10);
-
-        mockMvc.perform(post("/films")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(objectMapper.writeValueAsString(validFilm)))
-                .andExpect(status().isBadRequest())
-                .andExpect(jsonPath("$.error").exists());
-    }
-
-    @Test
-    void createFilmWithZeroDurationShouldReturnBadRequest() throws Exception {
-        validFilm.setDuration(0);
-
-        mockMvc.perform(post("/films")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(objectMapper.writeValueAsString(validFilm)))
-                .andExpect(status().isBadRequest())
-                .andExpect(jsonPath("$.error").exists());
-    }
-
-    @Test
-    void updateFilmShouldReturnOk() throws Exception {
-        Film updatedFilm = new Film();
-        updatedFilm.setId(1);
-        updatedFilm.setName("Updated Film Name");
-        updatedFilm.setDescription("A valid film description");
-        updatedFilm.setReleaseDate(LocalDate.of(2000, 1, 1));
-        updatedFilm.setDuration(120);
-        updatedFilm.setMpa(mpa);
-        updatedFilm.setGenres(genres);
-
-        when(filmService.updateFilm(any(Film.class))).thenReturn(updatedFilm);
-
-        mockMvc.perform(put("/films")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(objectMapper.writeValueAsString(updatedFilm)))
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("$.name").value("Updated Film Name"))
-                .andExpect(jsonPath("$.mpa.name").value("PG-13"));
-    }
-
-    @Test
-    void createFilmWithEmptyBodyShouldReturnBadRequest() throws Exception {
-        mockMvc.perform(post("/films")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content("{}"))
-                .andExpect(status().isBadRequest())
-                .andExpect(jsonPath("$.error").exists());
-    }
-
-    @Test
-    void updateNonExistentFilmShouldThrowException() throws Exception {
-        validFilm.setId(999);
-        when(filmService.updateFilm(any(Film.class)))
-                .thenThrow(new NotFoundException("Фильм с id=999 не найден"));
-
-        mockMvc.perform(put("/films")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(objectMapper.writeValueAsString(validFilm)))
-                .andExpect(status().isNotFound())
-                .andExpect(jsonPath("$.error").exists());
-    }
-
-    @Test
     void getFilmById_ShouldReturnFilm() throws Exception {
-        Film createdFilm = new Film();
-        createdFilm.setId(1);
-        createdFilm.setName("Valid Film");
-        createdFilm.setDescription("A valid film description");
-        createdFilm.setReleaseDate(LocalDate.of(2000, 1, 1));
-        createdFilm.setDuration(120);
-        createdFilm.setMpa(mpa);
-        createdFilm.setGenres(genres);
-
-        when(filmService.getFilmById(1)).thenReturn(createdFilm);
+        when(filmService.getFilmById(1)).thenReturn(filmResponse);
 
         mockMvc.perform(get("/films/{id}", 1))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.id").value(1))
-                .andExpect(jsonPath("$.name").value("Valid Film"))
-                .andExpect(jsonPath("$.mpa.name").value("PG-13"));
+                .andExpect(jsonPath("$.name").value("Valid Film"));
     }
 
     @Test
@@ -288,25 +153,23 @@ class FilmControllerTest {
     }
 
     @Test
-    void updateGenres_ShouldReturnOk() throws Exception {
-        when(filmService.updateGenres(anyInt(), anySet())).thenReturn(validFilm);
+    void updateFilm_ShouldReturnUpdatedFilm() throws Exception {
+        FilmResponse updatedResponse = new FilmResponse();
+        updatedResponse.setId(1L);
+        updatedResponse.setName("Updated Film");
+        updatedResponse.setDescription("Updated description");
+        updatedResponse.setReleaseDate(LocalDate.of(2000, 1, 1));
+        updatedResponse.setDuration(120);
+        updatedResponse.setMpa(mpa);
+        updatedResponse.setGenres(responseGenres);
 
-        mockMvc.perform(put("/films/{id}/genres", 1)
+        when(filmService.updateFilm(any(UpdateFilmRequest.class))).thenReturn(updatedResponse);
+
+        mockMvc.perform(put("/films")
                         .contentType(MediaType.APPLICATION_JSON)
-                        .content(objectMapper.writeValueAsString(genres)))
+                        .content(objectMapper.writeValueAsString(updateRequest)))
                 .andExpect(status().isOk())
-                .andExpect(jsonPath("$.genres[0].name").value("Комедия"));
-    }
-
-    @Test
-    void updateMpa_ShouldReturnOk() throws Exception {
-        when(filmService.updateMpa(anyInt(), any(Mpa.class))).thenReturn(validFilm);
-
-        mockMvc.perform(put("/films/{id}/mpa", 1)
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(objectMapper.writeValueAsString(mpa)))
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("$.mpa.name").value("PG-13"));
+                .andExpect(jsonPath("$.name").value("Updated Film"));
     }
 
     @Test
@@ -327,19 +190,29 @@ class FilmControllerTest {
 
     @Test
     void getPopular_ShouldReturnList() throws Exception {
-        when(filmService.getPopular(10)).thenReturn(List.of(validFilm));
+        when(filmService.getPopular(10)).thenReturn(List.of(filmResponse));
 
         mockMvc.perform(get("/films/popular"))
                 .andExpect(status().isOk())
-                .andExpect(jsonPath("$").isArray());
+                .andExpect(jsonPath("$[0].name").value("Valid Film"));
     }
 
     @Test
-    void getPopular_WithCountParameter_ShouldReturnLimitedList() throws Exception {
-        when(filmService.getPopular(5)).thenReturn(List.of(validFilm));
+    void getPopular_WithCountParameter_ShouldRespectCount() throws Exception {
+        when(filmService.getPopular(5)).thenReturn(List.of(filmResponse));
 
         mockMvc.perform(get("/films/popular?count=5"))
                 .andExpect(status().isOk())
-                .andExpect(jsonPath("$").isArray());
+                .andExpect(jsonPath("$[0].name").value("Valid Film"));
+    }
+
+    @Test
+    void createFilm_WithInvalidData_ShouldReturn400() throws Exception {
+        createRequest.setName("");
+
+        mockMvc.perform(post("/films")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(createRequest)))
+                .andExpect(status().isBadRequest());
     }
 }
