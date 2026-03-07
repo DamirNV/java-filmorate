badfed60fad8943a5218aef8de5fe0c79a2dea7a
Damir Vafin <damirvafinnailevich@gmail.com>
2026-03-08 01:23:59 +0300

    style: удалить неиспользуемый импорт Comparator

--- src/main/java/ru/yandex/practicum/filmorate/service/FilmService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/FilmService.java
@@ -15,7 +15,6 @@ import ru.yandex.practicum.filmorate.mapper.FilmMapper;
 import ru.yandex.practicum.filmorate.model.Film;
 import ru.yandex.practicum.filmorate.model.Mpa;
 
-import java.util.Comparator;
 import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
