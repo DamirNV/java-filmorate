1140eec085e73eabf6dbf0d8465d077d98af6a0f
Damir Vafin <damirvafinnailevich@gmail.com>
2026-03-08 01:21:59 +0300

    style: удалить неиспользуемый импорт NotNull

--- src/main/java/ru/yandex/practicum/filmorate/dto/film/UpdateFilmRequest.java
+++ src/main/java/ru/yandex/practicum/filmorate/dto/film/UpdateFilmRequest.java
@@ -1,6 +1,5 @@
 package ru.yandex.practicum.filmorate.dto.film;
 
-import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 import jakarta.validation.constraints.Size;
 import lombok.Data;
