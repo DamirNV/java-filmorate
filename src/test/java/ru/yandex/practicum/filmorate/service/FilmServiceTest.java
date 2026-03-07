247a9bdd0dfd240ff0c684f223c64fe4ddc371f1
Damir Vafin <damirvafinnailevich@gmail.com>
2026-03-07 22:55:11 +0300

    test: добавить тесты для FilmService с репозиториями

--- src/test/java/ru/yandex/practicum/filmorate/service/FilmServiceTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/service/FilmServiceTest.java
@@ -7,26 +7,40 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import ru.yandex.practicum.filmorate.controller.NotFoundException;
+import ru.yandex.practicum.filmorate.dal.repositories.FilmRepository;
+import ru.yandex.practicum.filmorate.dal.repositories.GenreRepository;
+import ru.yandex.practicum.filmorate.dal.repositories.MpaRepository;
+import ru.yandex.practicum.filmorate.dal.repositories.UserRepository;
+import ru.yandex.practicum.filmorate.dto.film.CreateFilmRequest;
+import ru.yandex.practicum.filmorate.dto.film.FilmResponse;
+import ru.yandex.practicum.filmorate.dto.film.UpdateFilmRequest;
 import ru.yandex.practicum.filmorate.model.Film;
 import ru.yandex.practicum.filmorate.model.Genre;
 import ru.yandex.practicum.filmorate.model.Mpa;
-import ru.yandex.practicum.filmorate.storage.film.FilmStorage;
-import ru.yandex.practicum.filmorate.storage.user.UserStorage;
+import ru.yandex.practicum.filmorate.model.User;
 
 import java.time.LocalDate;
 import java.util.*;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyInt;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 class FilmServiceTest {
 
     @Mock
-    private FilmStorage filmStorage;
+    private FilmRepository filmRepository;
 
     @Mock
-    private UserStorage userStorage;
+    private UserRepository userRepository;
+
+    @Mock
+    private GenreRepository genreRepository;
+
+    @Mock
+    private MpaRepository mpaRepository;
 
     @InjectMocks
     private FilmService filmService;
@@ -38,6 +52,12 @@ class FilmServiceTest {
     private Genre genre1;
     private Genre genre2;
     private Set<Genre> genres;
+    private User user;
+    private CreateFilmRequest createRequest;
+    private UpdateFilmRequest updateRequest;
+    private CreateFilmRequest.GenreId genreId1;
+    private CreateFilmRequest.GenreId genreId2;
+    private Set<CreateFilmRequest.GenreId> genreIds;
 
     @BeforeEach
     void setUp() {
@@ -56,6 +76,16 @@ class FilmServiceTest {
         genres = new LinkedHashSet<>();
         genres.add(genre1);
         genres.add(genre2);
+
+        genreId1 = new CreateFilmRequest.GenreId();
+        genreId1.setId(1);
+
+        genreId2 = new CreateFilmRequest.GenreId();
+        genreId2.setId(2);
+
+        genreIds = new LinkedHashSet<>();
+        genreIds.add(genreId1);
+        genreIds.add(genreId2);
 
         film1 = new Film();
         film1.setId(1);
@@ -86,195 +116,215 @@ class FilmServiceTest {
         film3.setLikes(new HashSet<>());
         film3.setMpa(mpa);
         film3.setGenres(new LinkedHashSet<>());
-    }
 
-    @Test
-    void addLike_ShouldAddUserIdToLikes() {
-        when(filmStorage.getById(1)).thenReturn(Optional.of(film1));
-        when(userStorage.getById(100)).thenReturn(Optional.of(mock(ru.yandex.practicum.filmorate.model.User.class)));
+        user = new User();
+        user.setId(100);
+        user.setEmail("user@test.com");
+        user.setLogin("user");
+        user.setName("User");
+        user.setBirthday(LocalDate.of(1990, 1, 1));
 
-        filmService.addLike(1, 100);
+        createRequest = new CreateFilmRequest();
+        createRequest.setName("New Film");
+        createRequest.setDescription("New Description");
+        createRequest.setReleaseDate(LocalDate.of(2000, 1, 1));
+        createRequest.setDuration(120);
+        createRequest.setMpa(mpa);
+        createRequest.setGenres(genreIds);
 
-        assertTrue(film1.getLikes().contains(100));
-        assertEquals(1, film1.getLikes().size());
-    }
-
-    @Test
-    void addLike_WhenFilmNotFound_ShouldThrowException() {
-        when(filmStorage.getById(1)).thenReturn(Optional.empty());
-
-        assertThrows(NotFoundException.class, () -> filmService.addLike(1, 100));
-        verify(filmStorage, times(1)).getById(1);
-        verify(userStorage, never()).getById(anyInt());
-    }
-
-    @Test
-    void addLike_WhenUserNotFound_ShouldThrowException() {
-        when(filmStorage.getById(1)).thenReturn(Optional.of(film1));
-        when(userStorage.getById(100)).thenReturn(Optional.empty());
-
-        assertThrows(NotFoundException.class, () -> filmService.addLike(1, 100));
-        assertFalse(film1.getLikes().contains(100));
-    }
-
-    @Test
-    void addLike_WhenAlreadyLiked_ShouldNotDuplicate() {
-        film1.getLikes().add(100);
-
-        when(filmStorage.getById(1)).thenReturn(Optional.of(film1));
-        when(userStorage.getById(100)).thenReturn(Optional.of(mock(ru.yandex.practicum.filmorate.model.User.class)));
-
-        filmService.addLike(1, 100);
-
-        assertEquals(1, film1.getLikes().size());
-    }
-
-    @Test
-    void removeLike_ShouldRemoveUserIdFromLikes() {
-        film1.getLikes().add(100);
-
-        when(filmStorage.getById(1)).thenReturn(Optional.of(film1));
-        when(userStorage.getById(100)).thenReturn(Optional.of(mock(ru.yandex.practicum.filmorate.model.User.class)));
-
-        filmService.removeLike(1, 100);
-
-        assertFalse(film1.getLikes().contains(100));
-    }
-
-    @Test
-    void removeLike_WhenNotLiked_ShouldDoNothing() {
-        when(filmStorage.getById(1)).thenReturn(Optional.of(film1));
-        when(userStorage.getById(100)).thenReturn(Optional.of(mock(ru.yandex.practicum.filmorate.model.User.class)));
-
-        filmService.removeLike(1, 100);
-
-        assertTrue(film1.getLikes().isEmpty());
-    }
-
-    @Test
-    void getPopular_ShouldReturnFilmsSortedByLikesDesc() {
-        film1.getLikes().addAll(Set.of(1, 2, 3));
-        film2.getLikes().addAll(Set.of(4, 5));
-        film3.getLikes().addAll(Set.of(6));
-
-        when(filmStorage.getAll()).thenReturn(List.of(film1, film2, film3));
-
-        List<Film> popular = filmService.getPopular(3);
-
-        assertEquals(3, popular.size());
-        assertEquals(film1, popular.get(0));
-        assertEquals(film2, popular.get(1));
-        assertEquals(film3, popular.get(2));
-    }
-
-    @Test
-    void getPopular_ShouldRespectCountParameter() {
-        film1.getLikes().addAll(Set.of(1, 2, 3));
-        film2.getLikes().addAll(Set.of(4, 5));
-        film3.getLikes().addAll(Set.of(6));
-
-        when(filmStorage.getAll()).thenReturn(List.of(film1, film2, film3));
-
-        List<Film> popular = filmService.getPopular(2);
-
-        assertEquals(2, popular.size());
-        assertEquals(film1, popular.get(0));
-        assertEquals(film2, popular.get(1));
-    }
-
-    @Test
-    void getPopular_WhenCountMoreThanFilms_ShouldReturnAllFilms() {
-        when(filmStorage.getAll()).thenReturn(List.of(film1, film2, film3));
-
-        List<Film> popular = filmService.getPopular(10);
-
-        assertEquals(3, popular.size());
-    }
-
-    @Test
-    void getPopular_WhenNoLikes_ShouldReturnAllFilms() {
-        when(filmStorage.getAll()).thenReturn(List.of(film1, film2, film3));
-
-        List<Film> popular = filmService.getPopular(5);
-
-        assertEquals(3, popular.size());
+        updateRequest = new UpdateFilmRequest();
+        updateRequest.setId(1L);
+        updateRequest.setName("Updated Film");
+        updateRequest.setDescription("Updated Description");
+        updateRequest.setReleaseDate(LocalDate.of(2000, 1, 1));
+        updateRequest.setDuration(120);
+        updateRequest.setMpa(mpa);
+        updateRequest.setGenres(genreIds);
     }
 
     @Test
     void getAllFilms_ShouldReturnAllFilms() {
-        when(filmStorage.getAll()).thenReturn(List.of(film1, film2));
+        when(filmRepository.findAll()).thenReturn(List.of(film1, film2));
 
-        List<Film> result = filmService.getAllFilms();
+        List<FilmResponse> result = filmService.getAllFilms();
 
         assertEquals(2, result.size());
-        verify(filmStorage, times(1)).getAll();
+        assertEquals("Film 1", result.get(0).getName());
+        verify(filmRepository, times(1)).findAll();
     }
 
     @Test
     void getFilmById_ShouldReturnFilm() {
-        when(filmStorage.getById(1)).thenReturn(Optional.of(film1));
+        when(filmRepository.findById(1)).thenReturn(Optional.of(film1));
 
-        Film result = filmService.getFilmById(1);
+        FilmResponse result = filmService.getFilmById(1);
 
-        assertEquals(film1, result);
-        verify(filmStorage, times(1)).getById(1);
+        assertEquals("Film 1", result.getName());
+        assertEquals(2, result.getGenres().size());
+        verify(filmRepository, times(1)).findById(1);
     }
 
     @Test
     void getFilmById_WhenNotFound_ShouldThrowException() {
-        when(filmStorage.getById(999)).thenReturn(Optional.empty());
+        when(filmRepository.findById(999)).thenReturn(Optional.empty());
 
         assertThrows(NotFoundException.class, () -> filmService.getFilmById(999));
     }
 
     @Test
-    void createFilm_ShouldAddFilm() {
-        when(filmStorage.add(any(Film.class))).thenReturn(film1);
+    void createFilm_ShouldCreateAndReturnFilm() {
+        when(mpaRepository.findById(1)).thenReturn(Optional.of(mpa));
+        when(genreRepository.findById(1)).thenReturn(Optional.of(genre1));
+        when(genreRepository.findById(2)).thenReturn(Optional.of(genre2));
+        when(filmRepository.save(any(Film.class))).thenReturn(film1);
 
-        Film result = filmService.createFilm(film1);
+        FilmResponse result = filmService.createFilm(createRequest);
 
-        assertEquals(film1, result);
-        verify(filmStorage, times(1)).add(film1);
+        assertNotNull(result);
+        assertEquals("Film 1", result.getName());
+        verify(mpaRepository, times(1)).findById(1);
+        verify(genreRepository, times(1)).findById(1);
+        verify(genreRepository, times(1)).findById(2);
+        verify(filmRepository, times(1)).save(any(Film.class));
     }
 
     @Test
-    void updateFilm_ShouldUpdateFilm() {
-        when(filmStorage.update(any(Film.class))).thenReturn(film1);
+    void createFilm_WhenMpaNotFound_ShouldThrowException() {
+        when(mpaRepository.findById(1)).thenReturn(Optional.empty());
 
-        Film result = filmService.updateFilm(film1);
-
-        assertEquals(film1, result);
-        verify(filmStorage, times(1)).update(film1);
+        assertThrows(NotFoundException.class, () -> filmService.createFilm(createRequest));
+        verify(filmRepository, never()).save(any(Film.class));
     }
 
     @Test
-    void updateGenres_ShouldUpdateFilmGenres() {
-        Set<Genre> newGenres = new LinkedHashSet<>();
-        newGenres.add(genre2);
+    void createFilm_WhenGenreNotFound_ShouldThrowException() {
+        when(mpaRepository.findById(1)).thenReturn(Optional.of(mpa));
+        when(genreRepository.findById(1)).thenReturn(Optional.of(genre1));
+        when(genreRepository.findById(2)).thenReturn(Optional.empty());
 
-        when(filmStorage.getById(1)).thenReturn(Optional.of(film1));
-        when(filmStorage.update(any(Film.class))).thenReturn(film1);
-
-        Film result = filmService.updateGenres(1, newGenres);
-
-        assertEquals(newGenres, result.getGenres());
-        verify(filmStorage, times(1)).getById(1);
-        verify(filmStorage, times(1)).update(film1);
+        assertThrows(NotFoundException.class, () -> filmService.createFilm(createRequest));
+        verify(filmRepository, never()).save(any(Film.class));
     }
 
     @Test
-    void updateMpa_ShouldUpdateFilmMpa() {
-        Mpa newMpa = new Mpa();
-        newMpa.setId(2);
-        newMpa.setName("R");
+    void createFilm_WithoutGenres_ShouldCreateFilm() {
+        createRequest.setGenres(null);
 
-        when(filmStorage.getById(1)).thenReturn(Optional.of(film1));
-        when(filmStorage.update(any(Film.class))).thenReturn(film1);
+        when(mpaRepository.findById(1)).thenReturn(Optional.of(mpa));
+        when(filmRepository.save(any(Film.class))).thenReturn(film1);
 
-        Film result = filmService.updateMpa(1, newMpa);
+        FilmResponse result = filmService.createFilm(createRequest);
 
-        assertEquals(newMpa, result.getMpa());
-        verify(filmStorage, times(1)).getById(1);
-        verify(filmStorage, times(1)).update(film1);
+        assertNotNull(result);
+        verify(genreRepository, never()).findById(anyInt());
+        verify(filmRepository, times(1)).save(any(Film.class));
+    }
+
+    @Test
+    void updateFilm_ShouldUpdateAndReturnFilm() {
+        when(filmRepository.findById(1)).thenReturn(Optional.of(film1));
+        when(mpaRepository.findById(1)).thenReturn(Optional.of(mpa));
+        when(genreRepository.findById(1)).thenReturn(Optional.of(genre1));
+        when(genreRepository.findById(2)).thenReturn(Optional.of(genre2));
+        when(filmRepository.update(any(Film.class))).thenReturn(film1);
+
+        FilmResponse result = filmService.updateFilm(updateRequest);
+
+        assertNotNull(result);
+        verify(filmRepository, times(1)).findById(1);
+        verify(filmRepository, times(1)).update(any(Film.class));
+    }
+
+    @Test
+    void updateFilm_WhenFilmNotFound_ShouldThrowException() {
+        when(filmRepository.findById(1)).thenReturn(Optional.empty());
+
+        assertThrows(NotFoundException.class, () -> filmService.updateFilm(updateRequest));
+        verify(filmRepository, never()).update(any(Film.class));
+    }
+
+    @Test
+    void updateFilm_WithoutId_ShouldThrowException() {
+        updateRequest.setId(null);
+
+        assertThrows(NotFoundException.class, () -> filmService.updateFilm(updateRequest));
+    }
+
+    @Test
+    void addLike_ShouldAddLike() {
+        when(filmRepository.findById(1)).thenReturn(Optional.of(film1));
+        when(userRepository.findById(100)).thenReturn(Optional.of(user));
+        doNothing().when(filmRepository).addLike(1, 100);
+
+        filmService.addLike(1, 100);
+
+        verify(filmRepository, times(1)).addLike(1, 100);
+    }
+
+    @Test
+    void addLike_WhenFilmNotFound_ShouldThrowException() {
+        when(filmRepository.findById(1)).thenReturn(Optional.empty());
+
+        assertThrows(NotFoundException.class, () -> filmService.addLike(1, 100));
+        verify(filmRepository, never()).addLike(anyInt(), anyInt());
+    }
+
+    @Test
+    void addLike_WhenUserNotFound_ShouldThrowException() {
+        when(filmRepository.findById(1)).thenReturn(Optional.of(film1));
+        when(userRepository.findById(100)).thenReturn(Optional.empty());
+
+        assertThrows(NotFoundException.class, () -> filmService.addLike(1, 100));
+        verify(filmRepository, never()).addLike(anyInt(), anyInt());
+    }
+
+    @Test
+    void removeLike_ShouldRemoveLike() {
+        when(filmRepository.findById(1)).thenReturn(Optional.of(film1));
+        doNothing().when(filmRepository).removeLike(1, 100);
+
+        filmService.removeLike(1, 100);
+
+        verify(filmRepository, times(1)).removeLike(1, 100);
+    }
+
+    @Test
+    void getPopular_ShouldReturnFilmsSortedByLikes() {
+        film1.getLikes().addAll(Set.of(1, 2, 3));
+        film2.getLikes().addAll(Set.of(4, 5));
+        film3.getLikes().addAll(Set.of(6));
+
+        when(filmRepository.findAll()).thenReturn(List.of(film1, film2, film3));
+
+        List<FilmResponse> popular = filmService.getPopular(3);
+
+        assertEquals(3, popular.size());
+        assertEquals("Film 1", popular.get(0).getName());
+        assertEquals("Film 2", popular.get(1).getName());
+        assertEquals("Film 3", popular.get(2).getName());
+    }
+
+    @Test
+    void getPopular_WithLimit_ShouldReturnLimitedList() {
+        film1.getLikes().addAll(Set.of(1, 2, 3));
+        film2.getLikes().addAll(Set.of(4, 5));
+        film3.getLikes().addAll(Set.of(6));
+
+        when(filmRepository.findAll()).thenReturn(List.of(film1, film2, film3));
+
+        List<FilmResponse> popular = filmService.getPopular(2);
+
+        assertEquals(2, popular.size());
+        assertEquals("Film 1", popular.get(0).getName());
+        assertEquals("Film 2", popular.get(1).getName());
+    }
+
+    @Test
+    void getPopular_WhenNoLikes_ShouldReturnAllFilms() {
+        when(filmRepository.findAll()).thenReturn(List.of(film1, film2, film3));
+
+        List<FilmResponse> popular = filmService.getPopular(10);
+
+        assertEquals(3, popular.size());
     }
 }
